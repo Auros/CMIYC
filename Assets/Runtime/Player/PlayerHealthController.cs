bb1e44ab98fa0902dad448573eb4d622a4f1eaa4
Caeden117 <caeden.s@outlook.com>
2023-09-30 22:18:24 -0700

    player health + death

---
+++ Assets/Runtime/Player/PlayerHealthController.cs
@@ -0,0 +1,25 @@
+﻿using CMIYC.Projectiles;
+using UnityEngine;
+
+namespace CMIYC
+{
+    public class PlayerHealthController : MonoBehaviour, IProjectileTarget
+    {
+        [field: SerializeField]
+        public float InitialHealth { get; private set; } = 100f;
+
+        public float Health { get; private set; }
+
+        [SerializeField]
+        private DeathController _deathController = null!;
+
+        private void Start() => Health = InitialHealth;
+
+        public void OnProjectileHit(ProjectileHitEvent hitEvent)
+        {
+            Health -= hitEvent.Instance.Damage;
+
+            if (Health < 0) _deathController.Die();
+        }
+    }
+}
