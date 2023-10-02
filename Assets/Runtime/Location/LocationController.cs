6ead364e0041fb7e8f4c1723eb73894056ac2924
Caeden117 <caeden.s@outlook.com>
2023-10-01 19:15:12 -0700

    locations

---
+++ Assets/Runtime/Location/LocationController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace CMIYC.Location
+{
+    public class LocationController : MonoBehaviour
+    {
+        public event Action<string> OnLocationEnter;
+        public event Action<string> OnLocationExit;
+
+        private Stack<string> _locations = new();
+
+        private void Start()
+        {
+            var randomDrive = (char)UnityEngine.Random.Range('A', 'Z');
+            _locations.Push($"{randomDrive}:");
+        }
+
+        public string GetFullLocation()
+        {
+            StringBuilder stringBuilder = new();
+            // wahhhh allocations
+            foreach (var loc in _locations.ToArray().Reverse())
+            {
+                stringBuilder.Append(loc);
+                stringBuilder.Append('\\');
+            }
+            return stringBuilder.ToString();
+        }
+
+        public void EnterLocation(string location)
+        {
+            _locations.Push(location);
+            OnLocationEnter?.Invoke(location);
+        }
+
+        public void ExitLocation()
+        {
+            var location = _locations.Pop();
+            OnLocationExit?.Invoke(location);
+        }
+    }
+}
