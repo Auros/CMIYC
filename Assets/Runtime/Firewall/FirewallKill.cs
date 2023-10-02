708be4877db370884800791d0a04de75d4b759d6
Caeden117 <caeden.s@outlook.com>
2023-10-01 19:37:36 -0700

    firewall KILLS you

---
+++ Assets/Runtime/Firewall/FirewallKill.cs
@@ -0,0 +1,27 @@
+﻿using CMIYC.Player;
+using UnityEngine;
+
+namespace CMIYC
+{
+    public class FirewallKill : MonoBehaviour
+    {
+        [SerializeField]
+        private DeathController _deathController = null!;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.GetComponent<PlayerController>())
+            {
+                _deathController.Die();
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.GetComponent<PlayerController>())
+            {
+                _deathController.Die();
+            }
+        }
+    }
+}
